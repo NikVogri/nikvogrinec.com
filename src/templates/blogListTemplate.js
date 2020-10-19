a5668a3a12fc1d8b9fcdb607921c2e219c9fb439
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-10-19 20:20:58 +0200

    blog - footer stays on the bottom if there are no entries.

--- src/templates/blogListTemplate.js
+++ src/templates/blogListTemplate.js
@@ -40,8 +40,13 @@ const BlogPage = ({
         </p>
       </header>
       <hr />
-      <section className="md:grid grid-cols-3 gap-20 mt-8 py-5 ">
-        <div className="col-span-2 w-full ">
+      <section
+        className="md:grid grid-cols-3 gap-20 mt-8 py-5"
+        style={{
+          height: nodes.length < 1 ? "calc(100vh - (75px + 85px + 284px))" : "",
+        }}
+      >
+        <div className="col-span-2 w-full">
           {nodes.length < 1 && (
             <p className="no-found">No blogs found for this category...</p>
           )}
