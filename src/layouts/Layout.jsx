a9b9f6734326ea7579407bcb49e21377a9d3220c
NikVogri <vogrinec.nik@gmail.com>
2021-11-21 17:34:38 +0100

    update: remove particles from showing on the whole page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,12 +3,8 @@ import React, { useEffect, useContext } from "react"
 import "../styles/style.css"
 import Footer from "../components/Footer"
 import Navigation from "../components/Navigation"
-import Particles from "react-particles-js"
 
 import { myContext } from "../Context/ThemeContext"
-
-import particleConfigLight from "../config/particlesLight"
-import particleConfigDark from "../config/particlesDark"
 
 const Layout = ({ children }) => {
   const theme = useContext(myContext)
@@ -24,19 +20,11 @@ const Layout = ({ children }) => {
   }, [theme])
 
   return (
-    <myContext.Consumer>
-      {context => (
-        <>
-          <Particles
-            className="particles hidden md:block"
-            params={context.isDark ? particleConfigDark : particleConfigLight}
-          />
-          <Navigation />
-          <main className="container">{children}</main>
-          <Footer />
-        </>
-      )}
-    </myContext.Consumer>
+    <>
+      <Navigation />
+      <main className="container">{children}</main>
+      <Footer />
+    </>
   )
 }
 
