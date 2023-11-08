79aebfcf423e9651811dc1272f7c707d8f99cc7f
NikVogri <vogrinec.nik@gmail.com>
2023-11-08 18:19:43 +0100

    feat: add GDPR compliant TelemetryDeck analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,13 @@
+import React from "react"
 import Provider from "./src/Context/ThemeContext"
 
 export const wrapRootElement = Provider
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      src="https://cdn.telemetrydeck.com/websdk/telemetrydeck.min.js"
+      data-app-id="5B2883ED-92CC-4588-A8BC-88002875F5DC"
+    ></script>,
+  ])
+}
